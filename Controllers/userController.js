4b93c9029a08216717cf2da5a6f6f19cb289e8ad
Duc Anh <hoangducanh12a8@gmail.com>
2024-11-05 01:26:08 +0700

    done update user

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,18 +1,57 @@
 const User = require('../Models/user');
 
-
+// Lấy thông tin người dùng
 const getUser = async (req, res) => {
     try {
         const user = await User.findOne(); 
         if (!user) {
             return res.status(404).send('Người dùng không tồn tại');
         }
-        res.json(user); // Trả về dữ liệu người dùng
+        const successMessage = req.query.success ? 'Thay đổi thông tin tài khoản thành công!' : null; 
+        const errorMessage = req.query.error ? req.query.error : null; 
+        res.render('user', { user, successMessage, errorMessage }); 
     } catch (error) {
         res.status(500).send('Lỗi khi lấy dữ liệu người dùng');
     }
 };
 
+// cập nhật thông tin người dùng
+const updateUser = async (req, res) => {
+    const { username, phone, address, city, ward, district } = req.body;
+
+    // Kiểm tra điều kiện
+    const nameRegex = /^[^\d]*$/; 
+    const phoneRegex = /^[0-9]*$/; 
+
+    if (!nameRegex.test(username)) {
+        return res.redirect('/account?error=' + encodeURIComponent('Họ tên không được chứa chữ số.')); 
+    }
+    if (!phoneRegex.test(phone)) {
+        return res.redirect('/account?error=' + encodeURIComponent('Số điện thoại không được chứa kí tự.')); 
+    }
+
+    try {
+        const user = await User.findOne();
+        if (!user) {
+            return res.status(404).send('Người dùng không tìm thấy');
+        }
+
+        // Cập nhật thông tin người dùng
+        user.hoten = username;
+        user.sodienthoai = phone;
+        user.diachi = address;
+        user.thanhpho = city;
+        user.phuong = ward;
+        user.quan = district;
+
+        await user.save(); 
+        res.redirect('/account?success=true'); 
+    } catch (error) {
+        res.status(500).send('Lỗi khi cập nhật thông tin người dùng');
+    }
+};
+
 module.exports = {
     getUser,
+    updateUser,
 };
--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,19 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../Models/user'); // Import model User
-const path = require('path');
+const { getUser, updateUser } = require('../Controllers/userController'); 
 
 // Route cho trang thông tin tài khoản
-router.get('/account', async (req, res) => {
-    try {
-        const user = await User.findOne();
-        if (!user) {
-            return res.status(404).send('Người dùng không tìm thấy');
-        }
-        res.render('user', { user }); 
-    } catch (error) {
-        res.status(500).send('Lỗi khi lấy thông tin tài khoản');
-    }
-});
+router.get('/account', getUser); 
+
+// Route để cập nhật thông tin người dùng
+router.post('/account', updateUser); 
 
 module.exports = router;
